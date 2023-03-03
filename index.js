70ebdef966975a7b57f4b34c4db3f39fea79a385
lemreyes <67998813+lemreyes@users.noreply.github.com>
2023-03-03 20:49:19 +0800

    Part3 Ex3.8 Add object logging in morgan

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+// create custome message in the middleweare s
+morgan.token("log_obj", function (req) {
+  return `${JSON.stringify(req.body)}`;
+});
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(":method :url :status :response-time :req[header] :log_obj"));
 
 let persons = [
   {
